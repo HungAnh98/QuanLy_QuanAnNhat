61b0f836a3bdd1101ec8bf68c97eff6542224802
hai chau <chau.cine@gmail.com>
2018-11-17 17:39:42 +0700

    Them ham lay bang nhan vien

---
+++ QuanLyQuanAnNhat/DAO/NhanVien_DAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using DTO;
+
+namespace DAO
+{
+    public class NhanVien_DAO : Dataprovider
+    {
+        public DataTable getEmployeeTable()
+        {
+            try
+            {
+                //string sql = "";
+                return GetDataSet("NhanVien").Tables[0];
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
