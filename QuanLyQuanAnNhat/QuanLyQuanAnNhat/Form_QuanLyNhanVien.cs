0a9d3288b2fc3286567b32a4ce6ed47e19d3bc56
hai chau <chau.cine@gmail.com>
2018-11-17 18:13:19 +0700

    Tao ham clear() va getInfo() o form QLNV

--- QuanLyQuanAnNhat/QuanLyQuanAnNhat/Form_QuanLyNhanVien.cs
+++ QuanLyQuanAnNhat/QuanLyQuanAnNhat/Form_QuanLyNhanVien.cs
@@ -34,6 +34,40 @@ namespace QuanLyQuanAnNhat
             dgvNhanVien.DataSource = dt;
         }
 
+        private void clear()
+        {
+
+            txtSDT.Text = txtTenNV.Text = txtMaNV.Text = txtLuong.Text = txtChucVu.Text = "";
+            cbGioiTinh.Text = null;
+            dateTimePicker1.Value = DateTime.Parse("1/1/1990");
+
+        }
+
+        private NhanVien getInfo()
+        {
+            int maNV;
+            string ten, gioiTinh, chucVu, diaChi;
+            DateTime ngaySinh;
+            int sdt;
+            int luong;
+            if (flag == 0)
+            {
+                maNV = int.Parse(txtMaNV.Text);
+            }
+            else
+                maNV = (int.Parse(dgvNhanVien.Rows[dgvNhanVien.Rows.Count - 1].Cells["MaNV"].Value.ToString())) + 1;
+            ten = txtTenNV.Text;
+            ngaySinh = dateTimePicker1.Value.Date;//DateTime.Now.Date;
+            sdt = int.Parse(txtSDT.Text);
+            chucVu = txtChucVu.Text;
+            gioiTinh = cbGioiTinh.Text;
+            luong = int.Parse(txtLuong.Text);
+            diaChi = txtDiaChi.Text;
+
+            NhanVien nv = new NhanVien(maNV, ten, gioiTinh, ngaySinh, chucVu, luong, sdt, diaChi);
+            return nv;
+        }
+
         private void dgvNhanVien_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
